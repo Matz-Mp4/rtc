5b8722b2308e6502006254fe3fa0eb98a7846171
Matz <mateus.assalti@gmail.com>
2023-06-12 10:11:04 -0300

    Created and implemented color.rs

---
+++ src/color.rs
@@ -0,0 +1,74 @@
+use std::ops::{Add, Div, Mul, Sub};
+pub struct Color {
+    red: f64,
+    green: f64,
+    blue: f64,
+}
+
+impl Color {
+    pub fn new(red: f64, green: f64, blue: f64) -> Self {
+        Self { red, green, blue }
+    }
+}
+
+impl Sub for Color {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let red = self.red - rhs.red;
+        let green = self.green - rhs.green;
+        let blue = self.blue - rhs.blue;
+
+        Color { red, green, blue }
+    }
+}
+
+impl Add for Color {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let red = self.red + rhs.red;
+        let green = self.green + rhs.green;
+        let blue = self.blue + rhs.blue;
+
+        Color { red, green, blue }
+    }
+}
+
+//Scalar
+impl Mul<f64> for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        let red = self.red * rhs;
+        let green = self.green * rhs;
+        let blue = self.blue * rhs;
+
+        Color { red, green, blue }
+    }
+}
+
+//Scalar
+impl Div<f64> for Color {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        let red = self.red / rhs;
+        let green = self.green / rhs;
+        let blue = self.blue / rhs;
+
+        Color { red, green, blue }
+    }
+}
+
+impl Mul for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let red = self.red * rhs.red;
+        let green = self.green * rhs.green;
+        let blue = self.blue * rhs.blue;
+
+        Color { red, green, blue }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
 pub mod tuple;
+pub use tuple::point::Point;
+pub use tuple::tuple::Tuple;
+pub use tuple::vector::Vector;
+
+pub mod canvas;
+pub mod color;
+
